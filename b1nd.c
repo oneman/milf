24f5f04c4c75b11f58859d8e3626fdf49c5a366f
David Richards <rawdod@gmail.com>
2025-08-04 10:37:25 -0400

    ok

--- b1nd.c
+++ b1nd.c
@@ -45,19 +45,83 @@ subformats:
  mx:       0x0001 | <domain-name>
  txt: 
 
+  \256\205\  1\  0\0\1\0\0\0\0\0\0\nradcompany\2us\0\0\34\0\1           len=31
+  \256\205\201\200\0\1\0\0\0\1\0\0\nradcompany\2us\0\0\34\0\1\300
+
+
 */
+
+void printpacket(int sz, char *dat) {
+  for (int i = 0; i < sz; i++) {
+    u8 byte = dat[i];
+    for (u8 b = 0b10000000;b != 0; b = b >> 1) {
+      if (byte & b) {
+        printf("1");
+      } else {
+        printf("0");
+      }
+    }
+    printf(" %3u", byte);
+    if (isprint(byte)) {
+      printf(" %c", byte);
+    }
+    printf("\n");
+  }  
+}
+
+void parse(int sz, char *dat) {
+  printpacket(sz, dat);
+  if (sz < 12) return;
+  u16 id;
+  u16 qs;
+  u16 as;
+  u16 ns;
+  u16 aas;
+  revcp(&id, dat, 2);
+  revcp(&qs, &dat[4], 2);
+  revcp(&as, &dat[6], 2);
+  revcp(&ns, &dat[8], 2);
+  revcp(&aas, &dat[10], 2);
+  printf("id %u ", id);
+  if ((dat[3] & 0b00000000) == 0b00000000) {
+    printf("%u query ", qs);
+  }
+  if ((dat[3] & 0b10000000) == 0b10000000) {
+    printf("%u query %u response ", qs, as);
+    if ((dat[4] & 0b00001111) != 0b00000000) {
+      printf("error! ");
+    }
+  }
+  if (ns + aas) printf("%u ns %u aas\n", ns, aas);
+  char label[64];
+  int pos = 12;
+  for (int i = qs; i < qs; i++) {
+    for (;;) {
+      if (pos > sz) break;
+      int len = dat[pos];
+      if (!len) break;
+      printf("len %d ", len);
+      pos++;
+      zero(label, 45);
+      cp(label, &dat[pos], len);
+      pos += len;
+      printf("label %*s\n", len, label);
+    }
+  }
+}
 
 int main(int argc, char **argv) {
   if (argc > 1) printf("%d arguments retarded.\n", argc - 1);
-  int epfd = epoll_create1(0);
-  printf("epfd %d\n", epfd);
+  //int epfd = epoll_create1(0);
+  //printf("epfd %d\n", epfd);
   int ret = 0;
-  struct epoll_event ev;
+  //struct epoll_event ev;
   char addrstr[40];
   zero(addrstr, 40);
   struct sockaddr saddr;
   mset(&saddr, 0, sizeof(struct sockaddr));
   struct sockaddr_in *s = (struct sockaddr_in *)&saddr;
+  struct sockaddr_in6 *s6 = (struct sockaddr_in6 *)&saddr;
   s->sin_family = AF_INET;
   s->sin_port = htons(53);
   s->sin_addr.s_addr = htonl(INADDR_ANY);
@@ -69,11 +133,44 @@ int main(int argc, char **argv) {
   }
   ret = bind(sd, &saddr, sizeof(struct sockaddr_in));
   if (ret) { printf("bindfail: %s\n", strerror(errno)); return 4; }
-  ev.events = EPOLLIN;
-  ev.data.fd = sd;
-  ret = epoll_ctl(epfd, EPOLL_CTL_ADD, sd, &ev);
-  if (ret) { printf("epoll_ctlfail: %s\n", strerror(errno)); return 6; }
+  //ev.events = EPOLLIN;
+  //ev.data.fd = sd;
+  //ret = epoll_ctl(epfd, EPOLL_CTL_ADD, sd, &ev);
+  //if (ret) { printf("epoll_ctlfail: %s\n", strerror(errno)); return 6; }
   printf("b1nd sd %d listens for querys on port %d\n", sd, 53);
+  for (u64 i = 0;;i++) {
+    printf("b1nd loop %zu\n", i);
+    struct sockaddr from;
+    socklen_t rsz = sizeof(from);
+    char dat[4096];
+    ret = recvfrom(sd, dat, 4096, 0, &from, &rsz);
+    if (ret == 0) continue;
+    if (ret == -1) { printf("recvfromfail: %s\n", strerror(errno)); return 11; }
+    printf("%d bytes from ", ret);
+    if (rsz == sizeof(struct sockaddr_in)) {
+      s = (struct sockaddr_in *)&from;
+      u8 ip[4];
+      cp(ip, &s->sin_addr.s_addr, 4);
+      u16 port = 0;
+      revcp(&port, &s->sin_port, 2);
+      //printf("%d %u - ", ntohs(s->sin_port), port);
+      printf("%u.%u.%u.%u:%u\n", ip[0], ip[1], ip[2], ip[3], port);
+      parse(ret, dat);
+    }
+    if (rsz == sizeof(struct sockaddr_in6)) {
+      s6 = (struct sockaddr_in6 *)&from;
+      u8 ip6[16];
+      cp(ip6, &s6->sin6_addr.s6_addr, 16);
+      u16 port = 0;
+      revcp(&port, &s6->sin6_port, 2);
+      //printf("%d %u - ", ntohs(s6->sin6_port), port);
+      printf("ip6 %u:%u:%u:%u", ip6[0], ip6[1], ip6[2], ip6[3]);
+      printf(":%u:%u:%u:%u", ip6[4], ip6[5], ip6[6], ip6[7]);
+      printf(":%u:%u:%u:%u", ip6[8], ip6[9], ip6[10], ip6[11]);
+      printf(":%u:%u:%u:%u %u", ip6[12], ip6[13], ip6[14], ip6[15], port);
+    }
+    printf("\n");
+  }
   close(sd);
   return 2601;
 }
--- stdio.h
+++ stdio.h
@@ -10,6 +10,7 @@
 #include <net/if.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef float f32;
 typedef double f64;
